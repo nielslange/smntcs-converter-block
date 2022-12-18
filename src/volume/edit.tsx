82cfa6b75994d52003a976f8ef5d65cdd61a3e1e
Niels Lange <info@nielslange.de>
2022-12-18 15:34:49 +0700

    Minot volume conversion fixes

--- src/volume/edit.tsx
+++ src/volume/edit.tsx
@@ -27,28 +27,12 @@ export default function Block() {
 							<option value="dm³">Cubic Decimeters (dm³)</option>
 							<option value="m³">Cubic Meters (m³)</option>
 							<option value="km³">Cubic Kilometers (km³)</option>
-							{ /* <option value="in³">Cubic Inches (in³)</option>
-							<option value="ft³">Cubic Feet (ft³)</option>
-							<option value="yd³">Cubic Yards (yd³)</option>
-							<option value="mi³">Cubic Miles (mi³)</option>
-							<option value="tsp">Teaspoons (tsp)</option>
-							<option value="tbsp">Tablespoons (tbsp)</option>
-							<option value="fl oz">Fluid Ounces (fl oz)</option>
-							<option value="cup">Cups (cup)</option>
-							<option value="pint">Pints (pint)</option>
-							<option value="quart">Quarts (quart)</option>
-							<option value="gal">Gallons (gal)</option>
 							<option value="cl">Centilitres (cl)</option>
 							<option value="ml">Milliliters (ml)</option>
 							<option value="dl">Deciliters (dl)</option>
 							<option value="l">Liters (l)</option>
 							<option value="kl">Kiloliters (kl)</option>
 							<option value="hl">Hectoliters (hl)</option>
-							<option value="tsk">Teskedar (tsk)</option>
-							<option value="msk">Matskedar (msk)</option>
-							<option value="kkp">Kaffekoppar (kkp)</option>
-							<option value="glas">Glas (glas)</option>
-							<option value="kanna">Kannor (kanna)</option> */ }
 						</select>
 					</div>
 					<div>
@@ -61,33 +45,17 @@ export default function Block() {
 							<option value="dm³">Cubic Decimeters (dm³)</option>
 							<option value="m³">Cubic Meters (m³)</option>
 							<option value="km³">Cubic Kilometers (km³)</option>
-							{ /* <option value="in³">Cubic Inches (in³)</option>
-							<option value="ft³">Cubic Feet (ft³)</option>
-							<option value="yd³">Cubic Yards (yd³)</option>
-							<option value="mi³">Cubic Miles (mi³)</option>
-							<option value="tsp">Teaspoons (tsp)</option>
-							<option value="tbsp">Tablespoons (tbsp)</option>
-							<option value="fl oz">Fluid Ounces (fl oz)</option>
-							<option value="cup">Cups (cup)</option>
-							<option value="pint">Pints (pint)</option>
-							<option value="quart">Quarts (quart)</option>
-							<option value="gal">Gallons (gal)</option>
 							<option value="cl">Centilitres (cl)</option>
 							<option value="ml">Milliliters (ml)</option>
 							<option value="dl">Deciliters (dl)</option>
 							<option value="l">Liters (l)</option>
 							<option value="kl">Kiloliters (kl)</option>
 							<option value="hl">Hectoliters (hl)</option>
-							<option value="tsk">Teskedar (tsk)</option>
-							<option value="msk">Matskedar (msk)</option>
-							<option value="kkp">Kaffekoppar (kkp)</option>
-							<option value="glas">Glas (glas)</option>
-							<option value="kanna">Kannor (kanna)</option> */ }
 						</select>
 					</div>
 				</form>
 				<div className="result" id="volume-output">
-					0 °C = 32 °F
+					0 mm³ = 0 dm³
 				</div>
 			</Disabled>
 		</div>
